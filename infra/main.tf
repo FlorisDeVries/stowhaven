3f50c1f96932dbb19f2d9e25c0ed010885203377
Floris de Vries <florisdev@hotmail.com>
2025-09-07 21:40:10 +0200

    Back to name

--- infra/main.tf
+++ infra/main.tf
@@ -64,8 +64,9 @@ resource "azurerm_storage_account" "data" {
 
 # Container for backups
 resource "azurerm_storage_container" "backups" {
-  name               = "backups"
-  storage_account_id = azurerm_storage_account.data.id
+  name                  = "backups"
+  storage_account_name  = azurerm_storage_account.data.name
+  container_access_type = "private"
 }
 
 # Lifecycle management policy
